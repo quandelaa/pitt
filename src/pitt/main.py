52bc18300e5b2ae891a196811bb8eb564a0e608b
quandelaa <sergiotobamasilalahi@gmail.com>
2026-07-11 23:27:26 +0700

    write structure for the init and add commands

--- src/pitt/main.py
+++ src/pitt/main.py
@@ -1,2 +1,17 @@
+from getpass import getpass
+from .parse import parse
+
 def main():
-    
+    args = parse()  
+
+    if args.command == "init":
+        master_pass = getpass("master password: ")
+
+        print(master_pass)
+    elif args.command == "add":
+        service = args.service if args.service is not None else "-"
+        username = args.username if args.username is not None else "-"
+        note = args.note if args.note is not None else "-"
+
+        print(username, "on", service)
+        print(note)
