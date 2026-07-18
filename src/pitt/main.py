0d524bdcbba345474e412e185a7fa5409b1cb813
quandelaa <sergiotobamasilalahi@gmail.com>
2026-07-18 11:50:07 +0700

    add args.command function calling

--- src/pitt/main.py
+++ src/pitt/main.py
@@ -1,17 +1,14 @@
-from getpass import getpass
+from .functions import init
 from .parse import parse
 
-def main():
+def main() -> None:
     args = parse()  
 
     if args.command == "init":
-        master_pass = getpass("master password: ")
-
-        print(master_pass)
+        init()
     elif args.command == "add":
-        service = args.service if args.service is not None else "-"
-        username = args.username if args.username is not None else "-"
-        note = args.note if args.note is not None else "-"
-
-        print(username, "on", service)
-        print(note)
+        print(args.service)
+        print(args.username)
+        print(args.note)
+    elif args.command == "get":
+        print(args.service)
