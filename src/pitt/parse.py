b80971a94be23ca2be972a2a842c766fe07d7e53
quandelaa <sergiotobamasilalahi@gmail.com>
2026-07-18 11:50:44 +0700

    add arguments and parsers to introduce pitt add and pitt get commands

--- src/pitt/parse.py
+++ src/pitt/parse.py
@@ -5,11 +5,14 @@ def parse():
     subparser = parser.add_subparsers(dest="command", required=True, help="commands")
 
     subparser.add_parser("init", help="set up a master password to initialize the password pit of doom and despair")
-    new_pass_parser = subparser.add_parser("add", help="generate and store a new randomized password")
+    get_password_parser = subparser.add_parser("get", help="copy a stored password to the clipboard")
+    add_password_parser = subparser.add_parser("add", help="generate and store a new randomized password")
 
-    new_pass_parser.add_argument("-s", "--service", help="specify the service that the new generated password belongs to")
-    new_pass_parser.add_argument("-u", "--username", help="specify the username that the new generated password belongs to")
-    new_pass_parser.add_argument("-n", "--note", help="give notes about this particular password")
+    get_password_parser.add_argument("service", help="service name to copy the password that's saved to it")
+
+    add_password_parser.add_argument("-s", "--service", help="specify the service that the new generated password belongs to")
+    add_password_parser.add_argument("-u", "--username", help="specify the username that the new generated password belongs to")
+    add_password_parser.add_argument("-n", "--note", help="give notes about this particular password")
 
     args = parser.parse_args()
 
