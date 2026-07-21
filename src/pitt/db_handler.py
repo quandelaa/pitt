51e23ccb0b8b5227519ab17fdd3ddaf7b43e2c78
quandelaa <sergiotobamasilalahi@gmail.com>
2026-07-21 21:51:08 +0700

    feat: add a function to configure the saved master password hash

--- src/pitt/db_handler.py
+++ src/pitt/db_handler.py
@@ -1,9 +1,12 @@
 import sqlite3 as sql
+from .utils import get_db_path
 
-def init_db(db_path: str) -> None:
+def init_db() -> None:
     """
     Initialize the database that will store the encrypted passwords.
     """
+
+    db_path = str(get_db_path())
     
     conn = sql.connect(db_path)
     cur = conn.cursor()
@@ -16,6 +19,28 @@ def init_db(db_path: str) -> None:
               note TEXT
               password TEXT NOT NULL);"""
         )
-    
+
+    cur.executescript(
+        """CREATE TABLE IF NOT EXISTS master (
+              master_hash TEXT NOT NULL);"""
+        )
+
+    conn.commit()
+    conn.close()
+
+def configure_master_hash(new_hash: str) -> None:
+    db_path = str(get_db_path())
+
+    conn = sql.connect(db_path)
+    cur = conn.cursor()
+
+    cur.execute("SELECT COUNT(*) FROM master")
+    row_count = cur.fetchone()[0]
+
+    if row_count == 0:
+        cur.execute("INSERT INTO master (master_hash) VALUES ('')")
+
+    cur.execute("UPDATE master SET master_hash = ?", (new_hash,))
+
     conn.commit()
     conn.close()
