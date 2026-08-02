5133734ed78f54b1663784af60e6d335845dde88
quandelaa <sergiotobamasilalahi@gmail.com>
2026-08-02 16:07:37 +0700

    feat: create functions that helps the encryption and decryption of pitt add passwords

--- src/pitt/security.py
+++ src/pitt/security.py
@@ -1,14 +1,23 @@
-from argon2 import PasswordHasher, exceptions 
+from argon2 import PasswordHasher, exceptions, low_level, Type
 from cryptography.fernet import Fernet
+import base64
 import os
 
-def encrypt(password: str) -> str:
+def master_encrypt(password: str) -> str:
+    """
+    Returns the encrypted hash of a given master password
+    """
+
     kdf = PasswordHasher()
     hash = kdf.hash(password)
 
     return hash
 
-def verify_password(password: str, hash: str) -> bool:
+def verify_master_password(password: str, hash: str) -> bool:
+    """
+    Verifies whether the inputted master password matches with the stored master password
+    """
+    
     kdf = PasswordHasher()
 
     try:
@@ -17,3 +26,34 @@ def verify_password(password: str, hash: str) -> bool:
         return False
 
     return True
+
+def derive_key(password: str, salt: bytes) -> bytes:
+    """
+    Derives the 64-byte Argon2id key from the password
+    """
+
+    key = low_level.hash_secret_raw(password.encode("utf-8"), salt, 3, 65536, 1, 32, Type.ID)
+
+    b64_key = base64.urlsafe_b64encode(key)
+
+    return b64_key
+
+def encrypt(key: bytes, password: str) -> bytes:
+    """
+    Encrypts the given password using Fernet
+    """
+
+    f = Fernet(key)
+    token = f.encrypt(password.encode("utf-8"))
+
+    return token
+
+def decrypt(key: bytes, encrypted: bytes) -> bytes:
+    """
+    Decrypts the given encrypted password using Fernet
+    """
+
+    f = Fernet(key)
+    password = f.decrypt(encrypted)
+
+    return password
