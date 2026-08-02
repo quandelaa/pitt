3a21cfb8e2fa901354590e174cf3af3fb66389f8
quandelaa <sergiotobamasilalahi@gmail.com>
2026-08-02 16:06:01 +0700

    feat: create add() function to add a newly generated password into the database

--- src/pitt/functions.py
+++ src/pitt/functions.py
@@ -1,8 +1,9 @@
 from rich.console import Console
 from getpass import getpass
-from .db_handler import init_db, configure_master_hash
-from .security import encrypt, verify_password
-from .utils import get_db_path, check_db_exists, get_master_hash
+from os import urandom
+from .db_handler import init_db, configure_vault, store_password
+from .security import master_encrypt, verify_master_password, encrypt, decrypt, derive_key
+from .utils import get_db_path, check_db_exists, get_vault_property, create_password
 
 def init() -> None:
     """
@@ -13,27 +14,19 @@ def init() -> None:
 
     if check_db_exists() is True:
         db_path = get_db_path()
-        console.print(f"[bold yellow]:| your database is already set up at [italic]{db_path}[/]\n")
-
-        console.print("logging in..")
-        master_password = getpass("> master password: ")
-    
-        try:
-            master_hash = get_master_hash()
-        except RuntimeError as e:
-            console.print(f"[bold red]:( encountered error: {e}")
-            return
-
-        verify = verify_password(master_password, master_hash)
-        if verify is True:
-            console.print("\n[bold green]:) success!\n")
-        elif verify is False:
-            console.print("\n[bold red]:( passwords do not match, sorry -- aborting")
+        console.print(f"[bold yellow]:| your database was already initialized at [italic]{db_path}")
 
         return
 
-    master_password = getpass("> master password: ")
-    confirm = getpass("> enter the password again (confirm): ")
+    console.print("[bold yellow]! initializing..\n")
+
+    try:
+        master_password = getpass("> master password: ")
+        confirm = getpass("> enter the password again (confirm): ")
+    except KeyboardInterrupt:
+        # exit error message preventer
+        console.print("\n[bold red]bye!")
+        return
 
     if master_password != confirm:
         console.print("\n[bold red]:( passwords do not match, sorry -- aborting")
@@ -46,8 +39,53 @@ def init() -> None:
 
     console.print(f"[bold green]:) database set up at [italic]{db_path}")
 
-    new_hash = encrypt(master_password)
-    configure_master_hash(new_hash)
+    salt = urandom(16)
     
-    console.print("[bold green]:) master password successfully set up!")
-    console.print("\ndo pitt -h for help")
+    new_hash = master_encrypt(master_password)
+    configure_vault(salt, new_hash)
+    
+    console.print("[bold green]:) master password successfully set up!\n")
+    console.print("[bold yellow]! do pitt -h for help")
+
+def add(service: str | None, username: str | None, note: str | None) -> None:
+    """
+    Adds a password to the password vault with the given service, username and a note
+    """
+
+    console = Console()
+            
+    if service is None and username is None and note is None:
+        console.print("[bold red]:( you have to provide atleast a note, a username or a service")
+        return
+
+    try:
+        master_password = getpass("> master password: ")
+    except KeyboardInterrupt:
+        # exit error message preventer
+        console.print("\n[bold red]bye!")
+        return
+
+    try:
+        m_hash, salt = get_vault_property()
+    except Exception as e:
+        console.print(f"[bold red]:( error: {e}")
+        return
+
+    verify = verify_master_password(master_password, m_hash)
+    
+    if verify is False:
+        console.print("[bold red]:( wrong password!")
+        return
+
+    password = create_password()
+    console.print("\n[bold green]:) password created!")
+
+    key = derive_key(password, salt)
+    encrypted = encrypt(key, password)
+    
+    console.print("[bold green]:) password encrypted successfully!")
+    
+    store_password(service, username, note, encrypted)
+
+    console.print("[bold green]:) password stored in database successfully!")
+    console.print("\n[bold yellow]! do pitt -h for help")
