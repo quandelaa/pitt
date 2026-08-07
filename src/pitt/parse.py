fb8652884a446b2033b304cc9fe6e9ed30c0ddb0
quandelaa <sergiotobamasilalahi@gmail.com>
2026-08-07 18:52:03 +0700

    feat: create new arguments for pitt del

--- src/pitt/parse.py
+++ src/pitt/parse.py
@@ -11,8 +11,12 @@ def parse() -> argparse.Namespace:
     subparser.add_parser("init", help="set up a master password to initialize the password pit of doom and despair")
     subparser.add_parser("list", help="lists all the saved password")
 
+    del_password_parser = subparser.add_parser("del", help="delete a saved password")
     get_password_parser = subparser.add_parser("get", help="copy a stored password to the clipboard")
     add_password_parser = subparser.add_parser("add", help="generate and store a new randomized password")
+
+    del_password_parser.add_argument("-s", "--service", help="specify the service assigned to the password that is to be deleted")
+    del_password_parser.add_argument("-u", "--username", help="specify the username assigned to the password that is to be copied to clipboard")
 
     get_password_parser.add_argument("-s", "--service", help="specify the service assigned to the password that is to be copied to clipboard")
     get_password_parser.add_argument("-u", "--username", help="specify the username assigned to the password that is to be copied to clipboard")
