779243a68a74554e15594a93f5fbac4f1cec514d
quandelaa <sergiotobamasilalahi@gmail.com>
2026-08-15 22:52:49 +0700

    feat: add --force flag to del parser

--- src/pitt/parse.py
+++ src/pitt/parse.py
@@ -1,11 +1,11 @@
 import argparse
 
-def parse() -> argparse.Namespace:
+def parse() -> tuple:
     """
     Returns the parsed arguments
     """
 
-    parser = argparse.ArgumentParser(allow_abbrev=False, prog="pitt")
+    parser = argparse.ArgumentParser(allow_abbrev=False, prog="pitt", description="an extremely simple terminal-based password manager")
     subparser = parser.add_subparsers(dest="command", required=True, help="commands")
 
     subparser.add_parser("init", help="set up a master password to initialize the password pit of doom and despair")
@@ -16,7 +16,9 @@ def parse() -> argparse.Namespace:
     add_password_parser = subparser.add_parser("add", help="generate and store a new randomized password")
 
     del_password_parser.add_argument("-s", "--service", help="specify the service assigned to the password that is to be deleted")
-    del_password_parser.add_argument("-u", "--username", help="specify the username assigned to the password that is to be copied to clipboard")
+    del_password_parser.add_argument("-u", "--username", help="specify the username assigned to the password that is to be deleted")
+
+    del_password_parser.add_argument("-f", "--force", action="store_true", help="force the deletion of the selected password")
 
     get_password_parser.add_argument("-s", "--service", help="specify the service assigned to the password that is to be copied to clipboard")
     get_password_parser.add_argument("-u", "--username", help="specify the username assigned to the password that is to be copied to clipboard")
@@ -27,4 +29,10 @@ def parse() -> argparse.Namespace:
 
     args = parser.parse_args()
 
-    return args
+    subparsers = {
+        'get': get_password_parser,
+        'add': add_password_parser,
+        'del': del_password_parser,
+    }
+
+    return args, subparsers
