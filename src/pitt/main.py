e2d225776332b9e04089d3b1cb2e5fe1722aa69c
quandelaa <sergiotobamasilalahi@gmail.com>
2026-08-15 22:51:50 +0700

    chore: feat: doing pitt subcommand without a flag specified will print the help page of that csubcommand

--- src/pitt/main.py
+++ src/pitt/main.py
@@ -1,16 +1,28 @@
 from .functions import init, add, get, list_cmd, del_cmd
-from .parse import parse
+from .parse import parse 
 
 def main() -> None:
-    args = parse()  
+    args, subparsers = parse()  
 
     if args.command == "init":
         init()
     elif args.command == "add":
+        if (args.service, args.username, args.note) == (None, None, None):
+            subparsers[args.command].print_help()
+            return
+
         add(args.service, args.username, args.note)
     elif args.command == "get":
+        if (args.service, args.username) == (None, None):
+            subparsers[args.command].print_help()
+            return
+
         get(args.service, args.username)
     elif args.command == "list":
         list_cmd()
     elif args.command == "del":
-        del_cmd(args.service, args.username)
+        if (args.service, args.username) == (None, None):
+            subparsers[args.command].print_help()
+            return
+
+        del_cmd(args.service, args.username, args.force)
