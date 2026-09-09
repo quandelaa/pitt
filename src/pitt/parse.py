084d4371129a4fc557a1fed1e661341ec674e0b1
quandelaa <sergiotobamasilalahi@gmail.com>
2026-09-09 18:15:28 +0700

    feat: add flags and parsers for new export command and --import flag

--- src/pitt/parse.py
+++ src/pitt/parse.py
@@ -8,17 +8,21 @@ def parse() -> tuple:
     parser = argparse.ArgumentParser(allow_abbrev=False, prog="pitt", description="an extremely simple terminal-based password manager")
     subparser = parser.add_subparsers(dest="command", required=True, help="commands")
 
-    subparser.add_parser("init", help="set up a master password to initialize the password pit of doom and despair")
     subparser.add_parser("list", help="lists all the saved password")
 
+    init_password_parser = subparser.add_parser("init", help="set up a master password to initialize the password pit of doom and despair")
     del_password_parser = subparser.add_parser("del", help="delete a saved password")
     get_password_parser = subparser.add_parser("get", help="copy a stored password to the clipboard")
     add_password_parser = subparser.add_parser("add", help="generate and store a new randomized password")
+    exp_password_parser = subparser.add_parser("exp", help="exports all the saved passwords to a csv file")
 
     del_password_parser.add_argument("-s", "--service", help="specify the service assigned to the password that is to be deleted")
     del_password_parser.add_argument("-u", "--username", help="specify the username assigned to the password that is to be deleted")
 
     del_password_parser.add_argument("-f", "--force", action="store_true", help="force the deletion of the selected password")
+
+    exp_password_parser.add_argument("-p", "--path", help="the path to save the csv file to")
+    init_password_parser.add_argument("-i", "--importp", help="initialize the database from a csv file")
 
     get_password_parser.add_argument("-s", "--service", help="specify the service assigned to the password that is to be copied to clipboard")
     get_password_parser.add_argument("-u", "--username", help="specify the username assigned to the password that is to be copied to clipboard")
